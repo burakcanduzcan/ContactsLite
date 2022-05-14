a67ab6f6135fd972b67eaad9376271dc24757fce
Burakcan Duzcan <burakcanduzcan@gmail.com>
2022-05-14 12:45:45 +0300

    added splash screen from new splashscreen api

--- app/src/main/java/com/burakcanduzcan/contactslite/ui/main/MainActivity.kt
+++ app/src/main/java/com/burakcanduzcan/contactslite/ui/main/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.burakcanduzcan.contactslite.ui.main
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.burakcanduzcan.contactslite.R
 import com.burakcanduzcan.contactslite.databinding.ActivityMainBinding
 import com.burakcanduzcan.contactslite.databinding.ItemTabBinding
 import com.burakcanduzcan.contactslite.ui.contacts.ContactsFragment
+import com.burakcanduzcan.contactslite.ui.dialPad.DialPadFragment
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -24,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setViewPagerAdapter() {
         val adapter = MainViewPagerAdapter(this)
+        adapter.addFragment(DialPadFragment())
         adapter.addFragment(ContactsFragment())
         //...
         binding.viewPager.adapter = adapter
@@ -31,8 +36,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun configureTabs() {
         var tab = ItemTabBinding.inflate(layoutInflater)
-        tab.tvBaslik.text = getString(R.string.Contacts)
+        tab.ivLogo.setImageResource(R.drawable.ic_baseline_dialpad_24)
         binding.tabLayout.getTabAt(0)!!.customView = tab.root
+
+        tab = ItemTabBinding.inflate(layoutInflater)
+        tab.ivLogo.setImageResource(R.drawable.ic_baseline_person_24)
+        binding.tabLayout.getTabAt(1)!!.customView = tab.root
         //...
     }
 
