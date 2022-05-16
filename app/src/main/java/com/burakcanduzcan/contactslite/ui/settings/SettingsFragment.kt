104a04a15cf4f72c9d7b2dd6988933cffe96b294
Burakcan Duzcan <burakcanduzcan@gmail.com>
2022-05-16 23:55:35 +0300

    added SettingsFragment.kt and GroupsFragment.kt as blank fragments

---
+++ app/src/main/java/com/burakcanduzcan/contactslite/ui/settings/SettingsFragment.kt
@@ -0,0 +1,25 @@
+package com.burakcanduzcan.contactslite.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.burakcanduzcan.contactslite.R
+import com.burakcanduzcan.contactslite.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    private lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+}
