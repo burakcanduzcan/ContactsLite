180010dbfd87aa5204b3ea38ab7b49b8c0fd461a
Burakcan Duzcan <burakcanduzcan@gmail.com>
2022-05-29 00:58:46 +0300

    added default country settings to SettingsFragment.kt

--- app/src/main/java/com/burakcanduzcan/contactslite/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/burakcanduzcan/contactslite/ui/settings/SettingsFragment.kt
@@ -1,25 +1,39 @@
 package com.burakcanduzcan.contactslite.ui.settings
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.burakcanduzcan.contactslite.R
+import androidx.appcompat.app.AppCompatActivity
 import com.burakcanduzcan.contactslite.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
     private lateinit var binding: FragmentSettingsBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var pref: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    ): View {
+        binding = FragmentSettingsBinding.inflate(layoutInflater)
+        pref = requireContext().getSharedPreferences(requireActivity().packageName, AppCompatActivity.MODE_PRIVATE)
+
+        setupExistingSettings()
+
+        return binding.root
+    }
+
+    private fun setupExistingSettings() {
+        val settings1: String = pref.getString("defaultCountry", "DEFAULT").toString()
+        binding.ccpDefaultCountry.setCountryForNameCode(settings1)
+        binding.btnSetting1.setOnClickListener {
+            pref.edit().putString("defaultCountry", binding.ccpDefaultCountry.selectedCountryNameCode).apply()
+            binding.btnSetting1.isEnabled = false
+        }
+        binding.ccpDefaultCountry.setOnCountryChangeListener {
+            binding.btnSetting1.isEnabled = (settings1 != binding.ccpDefaultCountry.selectedCountryNameCode)
+        }
     }
 }
